3d54c0ca41ec0e46c333a6bedb10325cd15c4b94
Joao Silva <joaosilva@MBP-de-Joao.home>
2021-03-04 17:49:03 +0000

    Knexfile configurations

--- src/database/migrations/000_create_points.ts
+++ src/database/migrations/000_create_points.ts
@@ -1,19 +1,19 @@
 import Knex from "knex";
 
 export async function up(knex: Knex) {
-    return knex.schema.createTable('points', table => {
-        table.increments('id').primary();
-        table.string('image').notNullable();
-        table.string('name').notNullable();
-        table.string('email', 100).notNullable();
-        table.string('whatsapp', 20).notNullable();
-        table.decimal('latitude').notNullable();
-        table.decimal('longitude').notNullable();
-        table.string('city').notNullable();
-        table.string('street').notNullable();
-    });
+  return knex.schema.createTable("points", (table) => {
+    table.increments("id").primary();
+    table.string("image").notNullable();
+    table.string("name").notNullable();
+    table.string("email", 100).notNullable();
+    table.string("whatsapp", 20).notNullable();
+    table.decimal("latitude").notNullable();
+    table.decimal("longitude").notNullable();
+    table.string("city").notNullable();
+    table.string("street").notNullable();
+  });
 }
 
 export async function down(knex: Knex) {
-    return knex.schema.dropTable('points');
+  return knex.schema.dropTable("points");
 }
